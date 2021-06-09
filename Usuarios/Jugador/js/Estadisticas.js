2c7f3eee3f9d49632028b170f49ff80522f84287
ElTomy <tomassosa1303@gmail.com>
2021-06-09 17:13:31 -0300

    Estadisticas

---
+++ Usuarios/Jugador/js/Estadisticas.js
@@ -0,0 +1,4 @@
+$( document ).ready(function(){
+    //sessionStorage.getItem('Usuario');
+    $("#Estadisticas").load('php/Estadisticas.php');
+});
--- servidor.php
+++ servidor.php
@@ -125,6 +125,7 @@ class servidor{
         $stmts = $conn->prepare($sql);
 
         $stmts->bind_param("iiissii",$Rondas,$ELO_Min,$ELO_Max,$Fecha_inicio,$Fecha_fin,$Numero_Participantes);
+        $stmts->execute();
     }
     function InfoEstadisticas($usuario){
         $conn = $this->conectar();
@@ -136,13 +137,12 @@ class servidor{
         if($stmts->execute()){
             $stmts->store_result();
             $stmts->bind_result($Usuario,$ELO,$Victorias,$Derrotas,$Tablas,$Coronaciones,$Comidas,$Menos_Tiempo,$Menos_Movimientos);
-            while($stmts->fetch()){
-                $data = array('ELO' => $ELO, 'usuario' => $Usuario, 'Victorias' => $Victorias, 'Derrotas' => $Derrotas, 'Tablas' => $Tablas, 'Coronaciones' => $Coronaciones, 'Comidas' => $Comidas, 'Menos_Tiempo' => $Menos_Tiempo, 'Menos_Movimientos' => $Menos_Movimientos);
-                $info[] = $data;
-            }
+            $stmts->fetch();
             $stmts->close();
+            return array($ELO,$Victorias,$Derrotas,$Tablas,$Coronaciones,$Comidas,$Menos_Tiempo,$Menos_Movimientos);
+
         }
-        return $info;
+        return false;
     }
     function AgregarEstadistica($Usuario,$ELO,$Victorias,$Derrotas,$Tablas,$Coronaciones,$Comidas,$Menos_Tiempo,$Menos_Movimientos){
         $conn = $this->conectar();
@@ -150,6 +150,7 @@ class servidor{
         $stmts = $conn->prepare($sql);
 
         $stmts->bind_param("isiiiiiii",$ELO,$Usuario,$Victorias,$Derrotas,$Tablas,$Coronaciones,$Comidas,$Menos_Tiempo,$Menos_Movimientos);
+        $stmts->execute();
     }
 }
 ?>
