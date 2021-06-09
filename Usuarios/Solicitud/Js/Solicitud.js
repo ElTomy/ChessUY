6c26960799b620868cae5a3b5fd9522d9e2aa6e9
Firpito <medicenfirpito@gmail.com>
2021-06-09 18:05:58 -0300

    PHP-mailer Solicitudes

--- Usuarios/Solicitud/Js/Solicitud.js
+++ Usuarios/Solicitud/Js/Solicitud.js
@@ -1,53 +1,50 @@
 var len = 0;
 var x = 0;
-var ci, usr, nom, ape, ins, mai, tip, año, con, nac,pas;
+var ci, usr, nom, ape, ins, mai, tip, año, con, nac, pas;
 $(document).ready(function () {
     $.ajax({
         type: "Post",
         url: "../Solicitud/PHP/Solicitud.php",
         success: function (response) {
             var solic = JSON.parse(response);
-            while (x == 0) {
+            while (x <= 0) {
                 if (solic[len].ci == null) {
                     x = 1;
+                } else {
+                    ci = solic[len].ci;
+                    usr = solic[len].usuario;
+                    nom = solic[len].Nombre;
+                    ape = solic[len].apellido;
+                    ins = solic[len].Institucion;
+                    mai = solic[len].Mail;
+                    tip = solic[len].tipo;
+                    año = solic[len].año;
+                    con = solic[len].Contacto;
+                    nac = solic[len].Nacimiento;
+                    pas = solic[len].Contraseña;
+                    document.getElementById("tablaprueba").insertRow(-1).innerHTML = '<td>' + ci + '</td> <td id="user' + len + '">' + usr + '</td> <td>' + nom + '</td> <td>' + ape + '</td> <td>' + ins + '</td> <td>' + mai + '</td> <td>' + tip + '</td><td><button onclick="aprobar(' + len + ',' + "'" + ci + "'" + ',' + "'" + usr + "'" + ',' + "'" + nom + "'" + ',' + "'" + ape + "'" + ',' + "'" + ins + "'" + ',' + "'" + mai + "'" + ',' + "'" + tip + "'" + ',' + "'" + año + "'" + ',' + "'" + con + "'" + ',' + "'" + nac + "'" + ',' + "'" + pas + "'" + ');">✓</button><button onclick="rechazar(' + len + ',' + "'" + usr + "'" + ',' + "'" + nom + "'" +  ',' + "'" + mai + "'" +');">✖</button></td>';
+                    len++;
                 }
-                ci = solic[len].ci;
-                usr = solic[len].usuario;
-                nom = solic[len].Nombre;
-                ape = solic[len].apellido;
-                ins = solic[len].Institucion;
-                mai = solic[len].Mail;
-                tip = solic[len].tipo;
-                año = solic[len].año;
-                con = solic[len].Contacto;
-                nac = solic[len].Nacimiento;
-                pas = solic[len].Contraseña;
-                document.getElementById("tablaprueba").insertRow(-1).innerHTML = '<td>' + ci + '</td> <td id="user' + len + '">' + usr + '</td> <td>' + nom + '</td> <td>' + ape + '</td> <td>' + ins + '</td> <td>' + mai + '</td> <td>' + tip + '</td><td><button onclick="aprobar(' + len + ',' + "'" + ci + "'" + ',' + "'" + usr + "'" + ',' + "'" + nom + "'" + ',' + "'" + ape + "'" + ',' + "'" + ins + "'" + ',' + "'" + mai + "'" + ',' + "'" + tip + "'" + ',' + "'" + año + "'" + ',' + "'" + con + "'" + ',' + "'" + nac + "'" +',' + "'" + pas + "'" + ');">✓</button><button onclick="rechazar(' + len + ',' + "'" + usr + "'" + ');">✖</button></td>';
-                len++;
             }
         }
     });
 });
 
-function aprobar(data, ci, usr, nom, ape, ins, mai, tip, año, con, nac,pas) {
-    console.log(data,ci, usr, nom, ape, ins, mai, tip, año, con, nac,pas);
-    alert("Usuario aprobado : " + usr);
+function aprobar(data, ci, usr, nom, ape, ins, mai, tip, año, con, nac, pas) {
     $.ajax({
         type: "Post",
         url: "../Solicitud/PHP/Aprobar.php",
-        data: { ci: ci, usuario: usr, nombre: nom, apellido: ape, institucion: ins, mail: mai, tipo: tip, año: año, contacto: con, nacimiento: nac, contraseña:pas },
+        data: { ci: ci, usuario: usr, nombre: nom, apellido: ape, institucion: ins, mail: mai, tipo: tip, año: año, contacto: con, nacimiento: nac, contraseña: pas },
         success: function (response) {
             console.log(response);
         }
     });
 }
-function rechazar(data, usr) {
-    console.log(data);
-    alert("Usuario rechazado : " + usr);
+function rechazar(data, usr, nom,mai) {
     $.ajax({
         type: "Post",
         url: "../Solicitud/PHP/Rechazar.php",
-        data: { usuario: usr },
+        data: { usuario: usr, nombre: nom ,mail:mai},
         success: function (response) {
             console.log(response);
         }
