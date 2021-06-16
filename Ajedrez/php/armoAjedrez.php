4c9ee47132aee47a432877651d80ed90b388ff0c
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2021-06-16 13:36:22 -0300

    Numeros + Texto Tablero Responsive

--- Ajedrez/php/armoAjedrez.php
+++ Ajedrez/php/armoAjedrez.php
@@ -1,25 +1,26 @@
 <?php
 $letras_y = array("A", "B", "C", "D", "E", "F", "G", "H");
+$numeros_x = array("8", "7", "6", "5", "4", "3", "2", "1");
 $Ajedrez = "";
 for($y = 1; $y <= 8; $y++){
     for($x = 1; $x <= 8; $x++){
         if($y == 1){
             if($x == 1){
-                $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell"><p class="board-number">' . $y . '</p><p class="board-text">' . $letras_y[($x - 1)] . '</p>';
+                $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell"><p class="board-number">' . $numeros_x[($y - 1)] . '</p><p class="board-text">' . $letras_y[($x - 1)] . '</p>';
             }else{
                 $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell"><p class="board-text">' . $letras_y[($x - 1)] . '</p>';
             }
 
         }else if(($y)%2 == 0){
             if($x == 1){
-                $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell2"><p class="board-number">' . $y . '</p>';
+                $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell2"><p class="board-number">' . $numeros_x[($y - 1)] . '</p>';
             }else{
                 $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell2">';
             }
 
         }else{
             if($x == 1){
-                $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell"><p class="board-number">' . $y . '</p>';
+                $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell"><p class="board-number">' . $numeros_x[($y - 1)] . '</p>';
             }else{
                 $Ajedrez .='<div onclick="seleccionar('.$x.','.$y.')" class="cell">';
             }
