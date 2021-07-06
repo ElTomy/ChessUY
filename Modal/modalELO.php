60f3b8a23e385ff54a271bb41fb75bbf1288a2bd
MatiRasnik <MatiRasnik@users.noreply.github.com>
2021-07-06 03:02:42 -0300

    elo

--- Modal/modalELO.php
+++ Modal/modalELO.php
@@ -1,6 +1,4 @@
-
 <?php
-
 /*
     Principiante -------------------->  600
     Aficionado ----------------------> 1500
@@ -21,13 +19,13 @@
                         <p>Que tan bueno te consideras?</p>
                         <div class='container'>
                         <div class='card'>
-                            <button>Principiante</button> 
-                            <button>Aficionado</button>
-                            <button>Semibuttonrofecional</button>
-                            <button>Candidato a Maestro (CM)</button>
-                            <button>Maestro FIDE (MF)</button>
-                            <button>Maestro Internacional (MI)</button>
-                            <button>Gran Maestro (GM)</button>
+                            <button onclick='guardoESTI(1)'>Principiante</button> 
+                            <button onclick='guardoESTI(2)'>Aficionado</button>
+                            <button onclick='guardoESTI(3)'>Semiprofecional</button>
+                            <button onclick='guardoESTI(4)'>Candidato a Maestro (CM)</button>
+                            <button onclick='guardoESTI(5)'>Maestro FIDE (MF)</button>
+                            <button onclick='guardoESTI(6)'>Maestro Internacional (MI)</button>
+                            <button onclick='guardoESTI(7)'>Gran Maestro (GM)</button>
                         </div>
                         </div>
 
