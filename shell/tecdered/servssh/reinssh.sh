3e16f1722e9d7800e742fa47b16450564d0f688e
SvenQuiring <svenpqiep@gmail.com>
2021-07-22 21:07:00 -0300

    mejor restart

--- shell/tecdered/servssh/reinssh.sh
+++ shell/tecdered/servssh/reinssh.sh
@@ -4,34 +4,15 @@
 echo "| Intentando reiniciar el servicio SSH...            |"
 echo "+----------------------------------------------------+"
 echo ""
-if systemctl stop sshd >/dev/null 2>&1
+if systemctl restart sshd >/dev/null 2>&1
 then
-    if systemctl start sshd >/dev/null 2>&1
-    then
-        echo "+-------------------------------+"
-        echo "| Ya se reinicio el servcio SSH |"
-        echo "+-------------------------------+"
-    else
-        echo "+--------------------------------------+"
-        echo "| No se pudo reiniciar el servicio SSH |"
-        echo "+--------------------------------------+"
-    fi
-elif systemctl start sshd >/dev/null 2>&1
-then
-    if system stop sshd >/dev/null 2>&1
-    then
-        echo "+-------------------------------+"
-        echo "| Ya se reinicio el servcio SSH |"
-        echo "+-------------------------------+"
-    else
-        echo "+--------------------------------------+"
-        echo "| No se pudo reiniciar el servicio SSH |"
-        echo "+--------------------------------------+"
-    fi
+    echo "+-------------------------------+"
+    echo "| Ya se reinicio el servcio SSH |"
+    echo "+-------------------------------+"
 else
-    echo "+---------------------------+"
-    echo "| Hubo un error desconocido |"
-    echo "+---------------------------+"
+    echo "+--------------------------------------+"
+    echo "| No se pudo reiniciar el servicio SSH |"
+    echo "+--------------------------------------+"
 fi
 sleep 3s
 . ./tecdered/servssh/servssh.sh
