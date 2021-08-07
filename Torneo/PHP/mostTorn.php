183c8e13bc5423de5c09d30ca285253b8eb42d7c
SvenQuiring <svenpqiep@gmail.com>
2021-08-07 16:07:24 -0300

    vamos a descartar la redundancia, valga la descartes

--- Torneo/PHP/mostTorn.php
+++ Torneo/PHP/mostTorn.php
@@ -15,32 +15,10 @@ if(count($torneos) < 1) {
     $sinTorn = true;
 } else {
     for($i=0;$i<count($torneos);$i++) {
-        /*if(str_replace('-', '', $torneos[$i]['Fecha_inicio']) > date('Ymd') && str_replace('-', '', $torneos[$i]['Fecha_inicio']) > date('Ymd')) {
-            echo "
-
-        <div class='torneo-left'>
-            <img src='/ChessUY/media/images/Trofeo.png' alt=''>
-            <div class='participantes'>
-                <p>". $torneos[$i]['Numero_Participantes'] ." <i class='fas fa-users'></i></p>
-            </div>
-        </div>
-        <div class='torneo-right'>
-            <div class='torneo-info'>
-                <h2>Nombre del Torneo</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus dolores ducimus exercitationem eius laborum saepe facilis molestiae officia tempore totam?</p>
-            </div>
-            <div class='torneo-buttons'>
-                <a href=''><i class='fas fa-plus-circle'></i> Unirse</a>
-                <a href='/ChessUY/Usuarios/EstadisticasTorneo.php'><i class='fas fa-chart-line'></i> Estadisticas</a>
-            </div>
-        </div>
-        
-        ";
-        } else {
-            $sinTorn = true;
-        }*/
-
-        $diff = str_replace('-', '', $torneos[$i]['Fecha_inicio']) - $fechaAct;
+        $comP = str_replace('-', ' ', $torneos[$i]['InicioTorneo']);
+        $comTornP = explode(' ', $comP);
+        $fechTornP = $comTornP[0].$comTornP[1].$comTornP[2];
+        $diff = $fechTornP - $fechaAct;
         if($prim) {
             $diffAnt = $diff;
             $j = $i;
