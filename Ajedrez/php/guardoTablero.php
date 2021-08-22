a2a09e1f51ecaeeac3548149a02e8a59bc84369f
MatiRasnik <MatiRasnik@users.noreply.github.com>
2021-08-22 13:09:18 -0300

    guardo tablero

--- Ajedrez/php/guardoTablero.php
+++ Ajedrez/php/guardoTablero.php
@@ -7,7 +7,10 @@ $tablero = $_POST["tablero"];
 if(isset($_POST['turno'])){
     $turno = $_POST['turno'];
 }else{$turno = 1;}
+if(isset($_POST['movimientos'])){
+    $movimientos = $_POST['movimientos'];
+}else{$movimientos = '';}
 //temp 1 
 //temp 2
-echo $server->guardoTablero($_SESSION['usuario'], $tablero, $turno); 
+echo $server->guardoTablero($_SESSION['usuario'], $tablero, $turno, $movimientos); 
 ?>
