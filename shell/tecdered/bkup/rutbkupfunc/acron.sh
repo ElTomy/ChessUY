f365b05214bc8382a679d95d493be14d09553452
SvenQuiring <svenpqiep@gmail.com>
2021-09-22 10:29:06 -0300

    avances

--- shell/tecdered/bkup/rutbkupfunc/acron.sh
+++ shell/tecdered/bkup/rutbkupfunc/acron.sh
@@ -4,3 +4,19 @@
 echo "| Enviando datos a crontab...                        |"
 echo "+----------------------------------------------------+"
 echo ""
+if $hrbkup == null
+then
+    acron="*/${minbkup} * "
+else if $hrbkup == "NE"
+then
+    acron="* * "
+else
+    acron="${arr[1]} ${arr[0]}"
+fi
+
+if [ -n "$diabkup" ]
+then
+    acron="${acron} * * * mysqldump -u root -p contra ChessUY ${tabkup} > ../BD/${tabkup}_backup.sql"
+    echo ${acron} >> crontab -e
+fi
+
