486770035da33ecd721d6be2c80f81df313ef066
SvenQuiring <svenpqiep@gmail.com>
2021-09-27 19:15:14 -0300

    boton arreglado

--- Torneo/PHP/jugarPart.php
+++ Torneo/PHP/jugarPart.php
@@ -2,30 +2,26 @@
 date_default_timezone_set("America/Montevideo");
 include '../../servidor.php';
 $server = new servidor();
-$partid = $server->TraigoPartidosTorneo();
 session_start();
 
 // Ver si esta en algun torneo y en cual
 if(isset($_SESSION['usuario'])) {
     $usuarioLog = $_SESSION['usuario'];
+    $partid = $server->InfoPartida($usuarioLog);
     for($i=0;$i<count($partid);$i++) {
-        if($partid[$i]['usu1'] == $usuarioLog || $partid[$i]['usu2'] == $usuarioLog && $partid[$i]['Torneo'] != 0) {
-            $idTornUnid = $partid[$i]['Torneo'];
+        if($partid[$i]['Jugador1'] == $usuarioLog || $partid[$i]['Jugador2'] == $usuarioLog && $partid[$i]['ID_Torneo'] != 0) {
+            $idTornUnid = $partid[$i]['ID_Torneo'];
             // Info adicional del torneo
             $infoAdi = $server->InfoTorneo();
-            for($i=0;$i<count($infoAdi);$i++) {
-                if($infoAdi[$i]['ID_Torneo'] == $idTornUnid) {
-                    $horaTornArr = explode(' ', str_replace('-', ' ', $infoAdi[$i]['InicioTorneo']));
+            for($j=0;$j<count($infoAdi);$j++) {
+                if($infoAdi[$j]['ID_Torneo'] == $idTornUnid) {
+                    $horaTornArr = explode(' ', str_replace('-', ' ', $infoAdi[$j]['InicioTorneo']));
                     $horaTorn = $horaTornArr[3];
-                    $i = count($infoAdi);
+                    $j = count($infoAdi);
                 }
             }
-            
-            // Ver si hay una partida para hoy
-            $tomyxd = $server->InfoPartida($idTornUnid); 
-            var_dump($tomyxd);
 
-            if($tomyxd[0]['Fecha'] == date('Y-m-d')) {
+            if($partid[$i]['Fecha'] == date('Y-m-d')) {
                 $timeDiff = str_replace(':', '', $horaTorn) - date('gis');
                 // Ver si la partida para hoy esta a 30 min de empezar
                 if($timeDiff < 3000 && $timeDiff > 0) {
