1074046203075e499de8cf652859dc7edd586608
MatiRasnik <MatiRasnik@users.noreply.github.com>
2021-09-28 11:21:55 -0300

    moto tryhard cagon

--- Torneo/js/tornFunc.js
+++ Torneo/js/tornFunc.js
@@ -405,7 +405,6 @@ function uniraBD(tornID) {
         type: "post",
         data: {tornID : tornID},
         success: function (exec) {
-            alert(exec);
             $.ajax({
                 url: "/ChessUY/Modal/unirTNotif.php",
                 type: "post",
