6ca9583229d9abce67ad5a8c4d3e9951b5ab1915
MatiRasnik <64052131+MatiRasnik@users.noreply.github.com>
2021-10-11 12:36:10 -0300

    traigo nuevo tablero

--- Ajedrez/php/traigoTablero.php
+++ Ajedrez/php/traigoTablero.php
@@ -2,5 +2,5 @@
 include '../../servidor.php';
 session_start();
 $server= new servidor();
-echo json_encode($server->TraigoTablero($_SESSION['usuario'])); 
+echo json_encode($server->TraigoTablero($_POST['id_partido'])); 
 ?>
