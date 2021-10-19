130d1bdaec3508996a1ff76c6525c5f4c9039046
SvenQuiring <svenpqiep@gmail.com>
2021-10-19 19:39:45 -0300

    mini cambios en boton jugar torneo

--- Torneo/PHP/jugarPart.php
+++ Torneo/PHP/jugarPart.php
@@ -11,11 +11,19 @@ if(isset($_SESSION['usuario'])) {
     for($i=0;$i<count($partid);$i++) {
         if($partid[$i]['Jugador1'] == $usuarioLog || $partid[$i]['Jugador2'] == $usuarioLog && $partid[$i]['ID_Torneo'] != 0) {
             $idTornUnid = $partid[$i]['ID_Torneo'];
+            // Info adicional del torneo
+            $infoAdi = $server->InfoTorneo();
+            for($j=0;$j<count($infoAdi);$j++) {
+                if($infoAdi[$j]['ID_Torneo'] == $idTornUnid) {
+                    $tempDesc = str_replace(':', '', $infoAdi[$j]['TiempoDescalificar']);
+                    $j = count($infoAdi);
+                }
+            }
             $fechahora = explode(' ', $partid[$i]['Fecha']);
             if($fechahora[0] == date('Y-m-d')) {
                 $timeDiff = str_replace(':', '', $fechahora[1]) - date('Gis');
                 // Ver si la partida para hoy esta a 30 min de empezar
-                if($timeDiff <= 7000 && $timeDiff >= 0) {
+                if($timeDiff <= 7000 && $timeDiff >= 0 - $tempDesc) {
                     // Mostrar boton
                     echo "
                     <a>
@@ -28,8 +36,8 @@ if(isset($_SESSION['usuario'])) {
                     ";       
                 }
             }elseif($fechahora[0] == date('Y-m-d', strtotime("tomorrow"))) {
-                // Ver si la partida para hoy esta a 30 min de empezar
-                if(str_replace(':', '', $fechahora[1]) <= 7000) {
+                // Ver si la partida para mañana esta a 30 min de empezar
+                if($timeDiff <= -233000) {
                     // Mostrar boton
                     echo "
                     <a>
